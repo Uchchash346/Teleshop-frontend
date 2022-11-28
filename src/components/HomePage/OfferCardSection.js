97a5cbff9a84a02b57279a4373a99dc1efcb863b
Uchchash346 <mru416@gmail.com>
2022-11-28 18:12:34 +0600

    Offer section setup successfully

--- src/components/HomePage/OfferCardSection.js
+++ src/components/HomePage/OfferCardSection.js
@@ -3,66 +3,36 @@ import React from 'react';
 const OfferCardSection = () => {
     return (
         <>
-            <div className='container mx-auto pt-5'>
-                <div className='grid grid-cols-3 gap-3'>
+            <div className='container mx-auto pt-5 max-w-7xl'>
+                <div className='grid grid-cols-1 px-3 lg:grid-cols-3 md:grid-cols-3 gap-3'>
                     <div className="bg-white shadow-2xl rounded-lg mb-6 tracking-wide" >
                         <div className="md:flex-shrink-0">
-                            <img src="https://ik.imagekit.io/q5edmtudmz/post1_fOFO9VDzENE.jpg" alt="mountains" className="w-full h-64 rounded-lg rounded-b-none" />
+                            <img src="https://teletalk.com.bd/storage/app/uploads/public/606/952/cb6/606952cb6fd53533586831.png" alt="mountains" className="w-full h-64 rounded-lg rounded-b-none" />
                         </div>
                         <div className="px-4 py-2 mt-2">
-                            <h2 className="font-bold text-2xl text-gray-800 tracking-normal">My Amaizing Journey to the Mountains.</h2>
-                            <p className="text-sm text-gray-700 px-2 mr-1">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora reiciendis ad architecto at aut placeat quia, minus dolor praesentium officia maxime deserunt porro amet ab debitis deleniti modi soluta similique...
-                            </p>
-                            <div className="flex items-center justify-between mt-2 mx-6">
-                                <a href="/" className="text-blue-500 text-xs -ml-3 ">Show More</a>
-                                <a href="/" className="flex text-gray-700">
-                                    <svg fill="none" viewBox="0 0 24 24" className="w-6 h-6 text-blue-500" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
-                                    </svg>
-                                    5
-                                </a>
-                            </div>
+                            <h2 className="font-bold text-2xl text-[#71BD44] tracking-normal">Uninterrupted, ad-free music anytime, anywhere</h2>
+                            <button href="/" className="text-[#71BD44] text-xl border-double border-4 border-[#71BD44] rounded-full w-32 mt-2">Click Here</button>
+
                         </div>
                     </div>
                     <div className="bg-white shadow-2xl rounded-lg mb-6 tracking-wide" >
                         <div className="md:flex-shrink-0">
-                            <img src="https://ik.imagekit.io/q5edmtudmz/post1_fOFO9VDzENE.jpg" alt="mountains" className="w-full h-64 rounded-lg rounded-b-none" />
+                            <img src="https://teletalk.com.bd/storage/app/uploads/public/606/953/4a3/6069534a3891b260477842.jpg" alt="mountains" className="w-full h-64 rounded-lg rounded-b-none" />
                         </div>
                         <div className="px-4 py-2 mt-2">
-                            <h2 className="font-bold text-2xl text-gray-800 tracking-normal">My Amaizing Journey to the Mountains.</h2>
-                            <p className="text-sm text-gray-700 px-2 mr-1">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora reiciendis ad architecto at aut placeat quia, minus dolor praesentium officia maxime deserunt porro amet ab debitis deleniti modi soluta similique...
-                            </p>
-                            <div className="flex items-center justify-between mt-2 mx-6">
-                                <a href="/" className="text-blue-500 text-xs -ml-3 ">Show More</a>
-                                <a href="/" className="flex text-gray-700">
-                                    <svg fill="none" viewBox="0 0 24 24" className="w-6 h-6 text-blue-500" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
-                                    </svg>
-                                    5
-                                </a>
-                            </div>
+                            <h2 className="font-bold text-2xl text-[#71BD44] tracking-normal">Get doctor's care on the go, round the clock</h2>
+                            <button href="/" className="text-[#71BD44] text-xl border-double border-4 border-[#71BD44] rounded-full w-32 mt-2">Click Here</button>
+
                         </div>
                     </div>
                     <div className="bg-white shadow-2xl rounded-lg mb-6 tracking-wide" >
                         <div className="md:flex-shrink-0">
-                            <img src="https://ik.imagekit.io/q5edmtudmz/post1_fOFO9VDzENE.jpg" alt="mountains" className="w-full h-64 rounded-lg rounded-b-none" />
+                            <img src="https://teletalk.com.bd/storage/app/uploads/public/606/953/b5c/606953b5c25c3950742306.png" alt="mountains" className="w-full h-64 rounded-lg rounded-b-none" />
                         </div>
                         <div className="px-4 py-2 mt-2">
-                            <h2 className="font-bold text-2xl text-gray-800 tracking-normal">My Amaizing Journey to the Mountains.</h2>
-                            <p className="text-sm text-gray-700 px-2 mr-1">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora reiciendis ad architecto at aut placeat quia, minus dolor praesentium officia maxime deserunt porro amet ab debitis deleniti modi soluta similique...
-                            </p>
-                            <div className="flex items-center justify-between mt-2 mx-6">
-                                <a href="/" className="text-blue-500 text-xs -ml-3 ">Show More</a>
-                                <a href="/" className="flex text-gray-700">
-                                    <svg fill="none" viewBox="0 0 24 24" className="w-6 h-6 text-blue-500" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
-                                    </svg>
-                                    5
-                                </a>
-                            </div>
+                            <h2 className="font-bold text-2xl text-[#71BD44] tracking-normal">Experience the magical world of toys</h2>
+                            <button href="/" className="text-[#71BD44] text-xl border-double border-4 border-[#71BD44] rounded-full w-32 mt-2">Click Here</button>
+
                         </div>
                     </div>
                 </div>
