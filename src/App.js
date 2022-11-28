f6b123518cd36a0b0ba0a0f86c3d057a4cefaf58
Uchchash346 <mru416@gmail.com>
2022-11-28 12:22:30 +0600

    Banner setup successfully

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/common_pages/Navbar';
 import TopNotificationBar from './components/common_pages/TopNotificationBar';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
     <>
       <TopNotificationBar />
       <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/HomePage/Banner.js
@@ -0,0 +1,30 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "../../styles/styles.css";
+// import required modules
+import { Pagination } from "swiper";
+
+// Banner images
+import bannerImage1 from '../images/banner_images/ts-1.png';
+import bannerImage2 from '../images/banner_images/ts-2.jpg';
+
+const Banner = () => {
+    return (
+        <>
+            <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+                <SwiperSlide>
+                    <img src={bannerImage1} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={bannerImage2} alt="" />
+                </SwiperSlide>
+            </Swiper>
+        </>
+    );
+};
+
+export default Banner;
