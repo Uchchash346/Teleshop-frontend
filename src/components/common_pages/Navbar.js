b3de2c0fc0cbcfc935bb97d60b6320607f9b0953
Uchchash346 <mru416@gmail.com>
2022-12-07 18:34:34 +0600

    setup teleplan page

--- src/components/common_pages/Navbar.js
+++ src/components/common_pages/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
             <nav className="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
                 <div className="container flex flex-wrap items-center justify-between mx-auto">
                     <a href="/" className="flex items-center">
-                        <img src="http://teleshop.teletalk.com.bd/pub/media/logo/stores/1/Teleshop-Logo2_1.png" className="h-6 mr-3 sm:h-10" alt="Flowbite Logo" />
+                        <img src="https://i.ibb.co/PhK0QN5/Teleshop-Logo2-1-1.png" className="h-6 mr-3 sm:h-10" alt="Flowbite Logo" />
                     </a>
                     {/* <button data-collapse-toggle="navbar-multi-level" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-multi-level" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
